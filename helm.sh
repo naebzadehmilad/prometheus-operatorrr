d43f34abd8a7a26c78e42df34b455bd842d890f4
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-01-29 03:09:16 +0330

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,1 +1,2 @@
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm install --namespace monitoring --name doks-cluster-monitoring -f custom-values.yaml stable/prometheus-operator
