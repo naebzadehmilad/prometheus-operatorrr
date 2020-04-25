b379ab28d5612bcbb51f3e96d21398b14b2d4c09
naebzadeh.milad@gmail.com <naebzadeh.milad@gmail.com>
2020-04-25 12:47:09 +0430

    ok

--- helm.sh
+++
@@ -1,2 +0,0 @@
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm install --namespace monitoring --name doks-cluster-monitoring -f custom-values.yaml stable/prometheus-operator
